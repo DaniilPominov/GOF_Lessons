ed1e295e8887a19f96703c42e3282373dcf4ca3a
Daniil Pominov <d.pominov2016@yandex.ru>
2026-03-30 19:51:50 +0700

    Add project files.

---
+++ GOF_Lessons/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GOF_Lessons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
